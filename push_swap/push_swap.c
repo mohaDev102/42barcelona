312f1f26d1193b5e07129cba0a3214d5dca61d67
Mohamed El Attar <mel-atta@car14s1.42barcelona.com>
2024-03-18 16:06:38 +0100

    control numbers sorted/ repeated

---
+++ push_swap/push_swap.c
@@ -0,0 +1,39 @@
+
+#include "push_swap.h"
+
+int main(int argc, char *argv[])
+{
+    if (argc >= 2)
+    {
+        int i;
+        int num;
+        t_stack *stack_a;
+
+        // t_stack *stack_a;
+        i = 1;
+        if (check_number(argv, argc))
+        {
+            write(1, "error\n", 6);
+            exit(1);
+        }
+        while (i < argc)
+        {
+            num = ft_atoi(argv[i]);
+            push_stack(&stack_a, num);
+            // is_sorted(num);
+            //check_number(num);
+            // push_stack(num1);
+            i++;
+        }
+        // print_stack(stack_a);
+        if (is_sorted(stack_a))
+        {
+            write(1, "ordenada", 8);
+            exit(0);
+        }
+        else
+            write(1, "no ordenada", 11);
+            return (0);
+
+    }
+}
