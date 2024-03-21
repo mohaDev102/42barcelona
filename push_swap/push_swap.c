2c054f318ae6b525ba4b90ce8c294db8f2e27abc
Mohamed El Attar <mel-atta@car13s4.42barcelona.com>
2024-03-21 15:52:21 +0100

    sort 3 rra ko

--- push_swap/push_swap.c
+++ push_swap/push_swap.c
@@ -5,34 +5,13 @@ int main(int argc, char *argv[])
 {
     if (argc >= 2)
     {
-        // int i;
-        // int num;
         t_stack *stack_a = NULL;
-
-        
-        // i = 1;
         if (check_number(argv, argc))
         {
             write(1, "error\n", 6);
             exit(1);
         }
-        
         push_stack(&stack_a, argv);
-        // while (i < argc)
-        // {
-        //     num = ft_atoi(argv[i]);
-        //     // push_stack(&stack_a, num);
-        //     // if (!new_node)
-        //     //     exit(1);
-        //     // new_node->next = stack_a;
-        //     // stack_a = new_node;
-        //     // is_sorted(num);
-        //     //check_number(num);
-        //     // push_stack(num1);
-        //     i++;
-        // }
-        // printf("first: %d\n", get_first_value(stack_a));
-        // printf("last: %d\n", get_last_value(stack_a));
         if (is_sorted(&stack_a))
         {
             // write(1, "ordenada", 8);
@@ -43,6 +22,5 @@ int main(int argc, char *argv[])
             sort_stack(&stack_a);
         print_stack(&stack_a);
         return (0);
-
     }
 }
--- push_swap/utils2.c
+++ push_swap/utils2.c
@@ -15,7 +15,6 @@ t_stack	*ft_lstlast(t_stack *lst)
 void	ft_lstaddend(t_stack **alst, t_stack *new)
 {
 	t_stack	*ptr;
-
 	ptr = *alst;
 	while (ptr->next)
 		ptr = ptr->next;
