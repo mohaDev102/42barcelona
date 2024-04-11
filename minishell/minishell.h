63409990164f5ae89c1b5ec38975146af27624e7
Mohamed El Attar <mel-atta@car10s4.42barcelona.com>
2024-04-11 16:58:01 +0200

    struct lexer

--- minishell/minishell.h
+++ minishell/minishell.h
@@ -8,13 +8,20 @@
 # include <stdlib.h>
 # include <unistd.h>
 
-typedef struct s_token
+typedef enum s_type
+{
+	NOTH = 0,
+	PIPE,
+	LESS,
+	LESS_L,
+}	t_type;
+
+typedef struct s_lexer
 {
 	char			*value;
-	int				is_space;
-	char			*token;
-	struct s_token	*next;
-}					t_token;
+	struct t_type	*type;
+	struct s_lexer	*next;
+}					t_lexer;
 
 int					ft_strncmp(const char *s1, const char *s2, size_t n);
 size_t				ft_strlen(const char *c);
@@ -25,5 +32,4 @@ char				*ft_strdup(const char *s1);
 void				*ft_calloc(size_t count, size_t size);
 void				*ft_memset(void *b, int c, size_t len);
 char				*ft_strcpy(char *s1, char *s2);
-
 #endif
