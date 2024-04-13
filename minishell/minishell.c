f5db2223f77b67b0559d0620c918e4615d720942
Mohamed El Attar <mel-atta@cbr9s2.42barcelona.com>
2024-04-13 17:58:24 +0200

    with lexer

--- minishell/minishell.c
+++ minishell/minishell.c
@@ -7,15 +7,16 @@ int main(void)
 
     while (1)
     {
-        line = readline("minishell >");
+        line = readline("minishell$ ");
         if (line)
             add_history(line);
         if (!ft_strncmp(line, "exit", 4))
         {
+            write(1, "exit\n", 5);
             free(line);
-            exit(1);
+            exit(0);
         }
-        printf("%s\n", line);
+        // ft_lexer(line);
     }
     free(line);
     return (0);
