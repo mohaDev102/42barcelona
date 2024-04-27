c5cf03b840f2aba241390ab394a6b8068f923285
Agathe Lounici <alounici@car13s4.42barcelona.com>
2024-04-27 20:10:19 +0200

    main + header

--- minishell/minishell.c
+++ minishell/minishell.c
@@ -1,13 +1,22 @@
 
 #include "minishell.h"
 
+// int g_return_value = 0;
+//t_signal_num signal_num;
+
 int main(void)
 {
     char *line;
+   // t_signal_num signal_num;
 
+receive_signal(0);
     while (1)
     {
+        receive_signal(0);
+        //write(1, "ici", 3);
         line = readline("minishell$ ");
+           // receive_signals_block();
+        //execve
         if (line)
             add_history(line);
         if (!ft_strncmp(line, "exit", 4))
@@ -17,7 +26,8 @@ int main(void)
             exit(0);
         }
         // ft_lexer(line);
+        //receive_signal();
     }
     free(line);
-    return (0);
+//    return (g_return_value2);
 }
--- minishell/minishell.h
+++ minishell/minishell.h
@@ -1,12 +1,22 @@
-
+#ifndef READLINE_LIBRARY
+#define READLINE_LIBRARY
+#endif
 #ifndef MINISHELL
 # define MINISHELL
 # include <limits.h>
-# include <readline/history.h>
-# include <readline/readline.h>
 # include <stdio.h>
+# include "readline/readline.h"
+# include "readline/history.h"
 # include <stdlib.h>
 # include <unistd.h>
+
+// int g_return_value2 = 0;
+
+// typedef struct s_signal_num
+// {
+// 	int return_value;
+// 	int sigindex;
+// }	t_signal_num;
 
 typedef enum s_type
 {
@@ -32,4 +42,6 @@ char				*ft_strdup(const char *s1);
 void				*ft_calloc(size_t count, size_t size);
 void				*ft_memset(void *b, int c, size_t len);
 char				*ft_strcpy(char *s1, char *s2);
+void 				receive_signal(int pid);
+void				receive_signals_block(void);
 #endif
