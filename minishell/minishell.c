7548b94093928d713cd0b7a69cd6bab0c4710ecb
Agathe Lounici <alounici@car13s4.42barcelona.com>
2024-04-27 20:24:21 +0200

    updated minishell.c

--- minishell/minishell.c
+++ minishell/minishell.c
@@ -13,24 +13,36 @@ int	main(int argc, char *argv[])
 		printf("Error\n");
 	}
 	receive_signal(1);
-    while (1)
-    {
-        receive_signal(0);
-        //write(1, "ici", 3);
-        line = readline("minishell$ ");
-           // receive_signals_block();
-        //execve
-        if (line)
-            add_history(line);
-        if (!ft_strncmp(line, "exit", 4))
-        {
-            write(1, "exit\n", 5);
-            free(line);
-            exit(0);
-        }
-        // ft_lexer(line);
-        //receive_signal();
-    }
-    free(line);
-//    return (g_return_value2);
+	while (1)
+	{
+		if (ft_operation(&lexer))
+			break ;
+		// line = readline("minishell$ ");
+		// if (line == NULL)
+		// {
+		// 	printf("exit\n");
+		// 	return (1);
+		// }
+		// if (ft_lexer(line, lexer) == 0)
+		// {
+		
+		// 	printf("no error?");
+		// 	// printf("%s", (*lexer)->value);
+		// 	// if (ft_parse())
+		// 	// {
+		// 	//     her_doc();
+		// 	//     if (!expandor())
+		// 	//         executor();
+		// 	// }
+		// }
+		// if (!ft_strncmp(line, "exit", 4))
+		// {
+		//     write(1, "exit\n", 5);
+		//     free(line);
+		//     exit(0);
+		// }
+		// ft_lexer(line);
+	}
+	// free(line);
+	return (g_error);
 }
