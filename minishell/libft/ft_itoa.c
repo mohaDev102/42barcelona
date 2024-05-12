ab98ab441e27cb1bfa2f226bf474580c352a20be
Mohamed El Attar <mel-atta@car11s4.42barcelona.com>
2024-05-12 12:45:29 +0200

    itoa for her_doc

---
+++ minishell/libft/ft_itoa.c
@@ -0,0 +1,64 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_itoa.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mel-atta <mel-atta@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/05/06 16:34:48 by mel-atta          #+#    #+#             */
+/*   Updated: 2024/05/06 16:34:57 by mel-atta         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../inc/minishell.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+static int	ft_contar_digitos(int n)
+{
+	int	i;
+
+	i = 0;
+	if (n <= 0)
+	{
+		i++;
+	}
+	while (n)
+	{
+		n /= 10;
+		i++;
+	}
+	return (i);
+}
+
+static int	ft_numero_absoluto(int n)
+{
+	if (n < 0)
+	{
+		n = -n;
+	}
+	return (n);
+}
+
+char	*ft_itoa(int n)
+{
+	char	*res;
+	int		len;
+
+	len = ft_contar_digitos(n);
+	res = (char *)malloc(sizeof(char) * (len + 1));
+	if (res == NULL)
+		return (NULL);
+	res[len] = '\0';
+	if (n == 0)
+		res[0] = '0';
+	else if (n < 0)
+		res[0] = '-';
+	while (n != 0)
+	{
+		res[--len] = ft_numero_absoluto(n % 10) + '0';
+		n /= 10;
+	}
+	return (res);
+}
