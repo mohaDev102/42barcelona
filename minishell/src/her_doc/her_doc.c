5dab2296558365f10a0d03e01299ca1a5be2223a
Agathe Lounici <alounici@car11s2.42barcelona.com>
2024-06-17 20:21:07 +0200

    fixed ctr c en herdoc

--- minishell/src/her_doc/her_doc.c
+++ minishell/src/her_doc/her_doc.c
@@ -6,19 +6,14 @@
 /*   By: alounici <alounici@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/21 11:58:34 by mel-atta          #+#    #+#             */
-/*   Updated: 2024/06/13 16:54:09 by alounici         ###   ########.fr       */
+/*   Updated: 2024/06/17 20:19:52 by alounici         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../inc/minishell.h"
 #include <termios.h>
-#include <setjmp.h>
-
-static struct {
-    sigjmp_buf jmpbuf;
-    struct termios saved_term_attr;
-} global_context;
-// static sigjmp_buf jmpbuf;
+#include <signal.h>
+#include <stdio.h>
 
 void	ft_putstr_fd(char *s, int fd)
 {
@@ -28,85 +23,79 @@ void	ft_putstr_fd(char *s, int fd)
 	write(fd, s++, len);
 }
 
-void handle_sigint_heredoc(int sig) {
-    (void)sig;
-    if (isatty(STDIN_FILENO))
-	{
-        tcsetattr(STDIN_FILENO, TCSANOW, &global_context.saved_term_attr);
-    }
-    write(STDOUT_FILENO, "\n", 1);
-    siglongjmp(global_context.jmpbuf, 1);
-}
-
-void	create_herdoc(char *limiter, char *path)
+void handle_sigint_heredoc(int sig)
 {
-	int		fd;
-	char	*str = NULL;
-	// pid_t	pid;
-	// int status;
+    struct termios  term_attr;
 
-
-	if (isatty(STDIN_FILENO)) 
+	if (sig == SIGINT)
 	{
-        tcgetattr(STDIN_FILENO, &global_context.saved_term_attr);
-    }
-    signal(SIGINT, handle_sigint_heredoc);
-    if (sigsetjmp(global_context.jmpbuf, 1) != 0) 
-	{
-        if (fd >= 0)
-			close(fd);
-        // if (str)
-		// 	free(str);
-        return;
-    }
-	fd = open(path, O_CREAT | O_WRONLY | O_TRUNC, 0666);
-	if (fd < 0)
-		return ;
-	//  receive_signal(1, 0); //, fd);
-	str = readline(" ");
-	while (str && ft_strcmp(limiter, str) != 0)
-	{
-		str = ft_strjoin(str, "\n");
-		ft_putstr_fd(str, fd);
-		free(str);
-		str = readline(" ");
-	// 	waitpid(pid, &status, 0);
-	// 		if (WIFEXITED(status))
-	// 			status = WEXITSTATUS(status);
-	// 		printf("1 = %d", status);
-	// 	if (WIFSIGNALED(status))
-	// 	{
-	// 		if (WTERMSIG(status) == SIGQUIT)
-	// 			status = 131;
-	// 		else if (WTERMSIG(status) == SIGINT)
-	// 		{	status = 130;
-	// 		close(fd);
-	// 		}
-	// 		printf("2 = %d", status);
-
-	// 	}
-	// 	if (status == 130)
-	// 	{
-	// 		write(1, "dans hd", 7);
-	// 	}
+		if (isatty(STDIN_FILENO))
+		{
+			if (tcgetattr(STDIN_FILENO, &term_attr) != -1)
+			{
+				term_attr.c_lflag |= ECHO;
+				tcsetattr(STDIN_FILENO, TCSANOW, &term_attr);
+			}
+		}
+		write(1, "ici", 3);
+		exit(130);
 	}
-	// exit_status(status);
-	close(fd);
-	free(str);
 }
 
-void	do_herdoc(t_redir *tmp, int i)
+void    create_herdoc(char *limiter, char *path)
 {
-	char	*num;
-	char	*path;
+    int     fd;
+    char    *str;
 
-	num = ft_itoa(i);
-	path = ft_strjoin("minishell", num);
-	free(num);
-	create_herdoc(tmp->file, path);
+    signal(SIGINT, handle_sigint_heredoc);
+    fd = open(path, O_CREAT | O_WRONLY | O_TRUNC, 0666);
+    if (fd < 0)
+        return ;
+    str = readline(" ");
+    while (str && ft_strcmp(limiter, str) != 0)
+    {
+        str = ft_strjoin(str, "\n");
+        ft_putstr_fd(str, fd);
+        free(str);
+        str = readline(" ");
+    }
+    close(fd);
+    free(str);
+    exit(0);
+}
+
+void    do_herdoc(t_redir *tmp, int i)
+{
+    char    *num;
+    char    *path;
+    pid_t   pid;
+	int status;
+
+	status = 0;
+    num = ft_itoa(i);
+    path = ft_strjoin("minishell", num);
+    pid = fork();
+    free(num);
+    if (pid == 0)
+	{
+    	create_herdoc(tmp->file, path);
+	}
+	waitpid(pid, &status, 0);
+	if (WIFEXITED(status))
+		status = WEXITSTATUS(status);
+	if (WIFSIGNALED(status))
+	{
+		if (WTERMSIG(status) == SIGQUIT)
+			status = 131;
+		else if (WTERMSIG(status) == SIGINT)
+		{
+			status = 130;
+		}
+	}
 	free(tmp->file);
-	tmp->file = ft_strdup(path);
-	free(path);
+    tmp->file = ft_strdup(path);
+	exit_status(status);
+    free(path);
 }
 
 void	her_doc(t_cmd *cmd)
@@ -115,7 +104,6 @@ void	her_doc(t_cmd *cmd)
 	t_redir	*tmp;
 
 	i = 0;
-	// 	return;
 	while (cmd != NULL)
 	{
 		tmp = cmd->redir;
