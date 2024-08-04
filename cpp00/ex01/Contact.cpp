a0a74360ef663007d1eaa7d7a096fccc34d4e47e
Mohamed El Attar <mel-atta@cbr14s5.42barcelona.com>
2024-08-04 02:29:59 +0200

    cpp

--- cpp00/ex01/Contact.cpp
+++
@@ -1,8 +0,0 @@
-#include "Contact.hpp"
-#include <iostream>
-#include <iomanip>
-
-void Contact::setFirstName(const std::string &name) {FirstName = name;}
-
-
-std::string Contact::getFirstName() const {return FirstName;}
